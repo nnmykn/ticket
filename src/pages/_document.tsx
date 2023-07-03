6b158a1008cf25904b9f63fe810b29b532dc7015
Kan Ninomiya <hi@kan.run>
2023-07-03 09:26:23 +0900

    OGP実装

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,13 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/yellow.png" />
         <title>Ticket</title>
+        <meta name="description" content="チケット販売システム" />
+        <meta property="og:url" content="https://ticket-picra.jp/" />
+        <meta property="og:title" content="Ticket" />
+        <meta property="og:site_name" content="Ticket" />
+        <meta property="og:description" content="チケット販売システム" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://ticket-picra.jp/ogp.png" />
       </Head>
       <body>
         <Main />
